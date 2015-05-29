155a4850b59ebca61038f65f51fc882eb2ee730f
ysc8620 <ysc8620@163.com>
2015-05-29 09:41:48 +0800

    ds

--- core/ThinkPHP/Common/functions.php
+++ core/ThinkPHP/Common/functions.php
@@ -142,7 +142,7 @@ function U($url, $params=array(), $redirect=false, $suffix=true) {
  */
 function parse_name($name, $type=0) {
     if ($type) {
-        return ucfirst(preg_replace("/_([a-zA-Z])/e", "strtoupper('\\1')", $name));
+        return (preg_replace("/_([a-zA-Z])/e", "strtoupper('\\1')", $name));
     } else {
         $name = preg_replace("/[A-Z]/", "_\\0", $name);
         return strtolower(trim($name, "_"));
