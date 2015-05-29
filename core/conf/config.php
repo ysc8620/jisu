8ea91c871a5497031075c0add5540eed0d021d96
ysc8620 <ysc8620@163.com>
2015-05-29 09:47:58 +0800

    debug

--- core/conf/config.php
+++ core/conf/config.php
@@ -50,7 +50,7 @@ $array = array(
 		'media'=>   array('30','Media Player'),
 		'real'=>    array('31','Real Player'),
 	),
-	//'APP_DEBUG'           =>true,    // 是否开启调试模式
+	'APP_DEBUG'           =>true,    // 是否开启调试模式
     //'SHOW_RUN_TIME'		=> true,   // 运行时间显示
     //'SHOW_ADV_TIME'		=> true,   // 显示详细的运行时间
     //'SHOW_DB_TIMES'		=> true,   // 显示数据库查询和写入次数	
