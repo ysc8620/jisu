2bf15a5170705a50d94a99b5f1c7f3f8f7ba2698
ysc8620 <ysc8620@163.com>
2015-06-07 11:01:01 +0800

    ds

--- runtime/js/hotkey.js
+++ runtime/js/hotkey.js
@@ -1,1 +1,6 @@
-document.write('<a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%871.html">热门影片1</a> <a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%872.html">热门影片2</a> <a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%873.html">热门影片3</a> <a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%874.html">热门影片4</a> <a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%875.html">热门影片5</a> <a href="/?s=vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%876.html">热门影片6</a>');
+document.write('<a href="/vod-search-wd-%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%871%0A%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%872%0A%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%873%0A%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%874%0A%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%875%0A%E7%83%AD%E9%97%A8%E5%BD%B1%E7%89%876-p-1.html">热门影片1
+热门影片2
+热门影片3
+热门影片4
+热门影片5
+热门影片6</a>');
