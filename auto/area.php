17253653198720efa36efe911d8bb11f951e0a08
ysc8620 <ysc8620@163.com>
2015-06-10 15:38:31 +0800

    ds

--- auto/area.php
+++ auto/area.php
@@ -10,10 +10,11 @@ if(!file_exists(  '../runtime/conf/config.php')){
     die('conf config no exists');
 }
 $config = include_once('../runtime/conf/config.php');
-if(!file_exists( '../runtime/data/_ppvod/list.php')){
-    die('list config no exists');
+if(!file_exists( '../runtime/data/_ppvod/area.php')){
+    die('arealist config no exists');
 }
-$list = @include_once('../runtime/data/_ppvod/list.php');
+$list = @include_once('../runtime/data/_ppvod/area.php');
+
 /**----------------------------------------------------------
  * 在数据列表中搜索
 +----------------------------------------------------------
@@ -43,6 +44,7 @@ function list_search($list,$condition) {
     }
     return $resultSet;
 }
+
 #print_r($config);
 $conn = mysql_connect($config['db_host'],$config['db_user'],$config['db_pwd']) or die('mysql connect error');
 mysql_select_db($config['db_name']);
@@ -51,33 +53,39 @@ $i = 0;
 $j=0;
 $size = 1000;
 do{
-    $sql = "SELECT vod_id, vod_class_name,vod_cid FROM js_vod WHERE vod_class='' AND length(vod_class_name)>0 ORDER BY vod_id ASC LIMIT $i, $size";
+    $sql = "SELECT vod_id, vod_area_name,vod_area,vod_cid FROM js_vod WHERE vod_area=0 AND length(vod_area_name)>0 ORDER BY vod_id ASC LIMIT $i, $size";
 
     $res = mysql_query($sql);
     $count = mysql_num_rows($res);
     echo $count."\r\n";
     if($count < 1){break;}
     while($row = mysql_fetch_assoc($res)){
-       $class = explode('/', $row['vod_class_name']);
-        $class_id = '';
-        if($class){
-            foreach($class as $i){
+       $area_name = explode('/', $row['vod_area_name']);
+        $area_id = '';
+        $data = list_search($list,"list_id={$row['vod_cid']}");
+        if($area_name){
+            foreach($area_name as $i){
                 $name = trim($i);
-                $data = list_search($list,"list_name={$name}");
                 if($data){
-                    $new_name = list_search($data,"list_pid={$row['vod_cid']}");
+                    $new_name = list_search($data,"name={$name}");
                     if($new_name){
-                        $new_cid = $new_name[0]['list_id'];
-                        $class_id .= $new_cid.'/';
+                        $area_id = $new_name[0]['id'];
+                        break;
+                    }else{
+                        $new_name = list_search($data,"alias_name=/{$name}/");
+                        if($new_name){
+                            $area_id = $new_name[0]['id'];
+                            break;
+                        }
                     }
 
                 }
             }
 
-            echo $row['vod_id'].'='.trim($class_id,'/').'   ='.($j++)."\r\n";
-            $vod_class = trim($class_id,'/');
-            if($vod_class){
-                mysql_query("UPDATE js_vod SET vod_class='{$vod_class}' WHERE vod_id='{$row['vod_id']}'");
+            echo $row['vod_id'].'='.$area_id.', '.$row['vod_cid'].',  '.$row['vod_area_name'].'='.($j++)."\r\n";
+           // $vod_class = trim($class_id,'/');
+            if($area_id){
+                mysql_query("UPDATE js_vod SET vod_area='{$area_id}' WHERE vod_id='{$row['vod_id']}'");
             }
         }
     }
