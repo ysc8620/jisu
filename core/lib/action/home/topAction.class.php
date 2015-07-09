9f879774e36c34e5f5d8414b0afb7cc4cf7253d4
ysc8620 <ysc8620@163.com>
2015-07-09 09:07:34 +0800

    ds

--- core/lib/action/home/topAction.class.php
+++ core/lib/action/home/topAction.class.php
@@ -22,11 +22,17 @@ class topAction extends homeAction{
         echo $data;
     }
     public function read(){
-		$mapname = !empty($_GET['id']) ? trim($_GET['id']):'rss';
-		$limit = !empty($_GET['limit']) ? intval($_GET['limit']):30;
-		$page = !empty($_GET['p']) ? intval($_GET['p']) : 1;
-		$this->assign('list_map',$this->Lable_Maps($mapname,$limit,$page));
-		$this->display('./public/maps/'.$mapname.'.html','utf-8','text/xml');
+        $list_dir= $_GET['list_dir'];
+        $id = $_GET['id'];
+        $cate = list_search(F('_ppvod/listtree'),'list_dir='.$list_dir);
+        //print_r($cate);
+        $this->assign('cate', $cate[0]);
+        $this->assign('id', $id);
+        $data = $this->fetch('top_read','utf-8','text/html');
+        if(true){
+            file_put_contents(ROOT_PATH . '/top/'.$list_dir.'-'.$id.'.html', $data);
+        }
+        echo $data;
 	}
 
 }
