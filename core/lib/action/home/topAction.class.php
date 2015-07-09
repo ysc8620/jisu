62b834ce3cc5e931350b49a09547946ac6a8d1a6
ysc8620 <ysc8620@163.com>
2015-07-09 09:15:51 +0800

    ds

--- core/lib/action/home/topAction.class.php
+++ core/lib/action/home/topAction.class.php
@@ -25,9 +25,13 @@ class topAction extends homeAction{
         $list_dir= $_GET['list_dir'];
         $id = $_GET['id'];
         $cate = list_search(F('_ppvod/listtree'),'list_dir='.$list_dir);
-        //print_r($cate);
+        $this->assign('list_dir', $list_dir);
         $this->assign('cate', $cate[0]);
         $this->assign('id', $id);
+
+        $cat = list_search( $cate[0]['son'], 'list_dir='.$id);
+
+        $this->assign('cat', $cat[0]);
         $data = $this->fetch('top_read','utf-8','text/html');
         if(true){
             file_put_contents(ROOT_PATH . '/top/'.$list_dir.'-'.$id.'.html', $data);
